95e85118d9f4c7cf46ec1cc845696e558dcb1dc3
Yordan Halachev <yordan.i.halachev@gmail.com>
2021-09-17 16:04:57 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -34,7 +34,7 @@ do curl -s \
 --header "Connection: keep-alive" \
 --header "Authorization: Bearer $TFE_TOKEN" \
 --header "Content-Type: application/vnd.api+json" \
-"https://app.terraform.io/api/v2/organizations/"$org_name"/workspaces?page%5Bsize%5D=100&page%5Bnumber%5D="$i"" | jq -r '.data[] .id' 
+"https://app.terraform.io/api/v2/organizations/"$org_name"/workspaces?page%5Bsize%5D=100&page%5Bnumber%5D="$i"" | jq -r '.data[] .id'  
 done >> list.txt
 
 #3.Create a main.tf file based on the workspace ID
@@ -87,11 +87,57 @@ cat main1.tf | sed "s/ -//g" | sed "s/> null//g" | sed -E '/^ +id +=|^ +operatio
 mv -f main2.tf main.tf
 
 rm -f main1.tf
+
+
+# Pottential issue: When you are dealing with a lot of workspaces , lets say over 200, while adding them there might be a problem like throtling
+<<COMMENT
+│ Error: Error getting client
+│ 
+│   with provider["registry.terraform.io/hashicorp/tfe"],
+│   on /Users/yhalachev/repos/API_tests/yordanh_paid/цлоне/tfe_workspace_config_creator/main.tf line 1, in provider "tfe":
+│    1: provider "tfe" {} 
+│ 
+│ Error getting client: Failed to request discovery document: Get "https://app.terraform.io/.well-known/terraform.json": dial tcp: lookup app.terraform.io on 8.8.8.8:53:
+│ read udp 192.168.0.102:59535->8.8.8.8:53: i/o timeout
+COMMENT
+
+cat main.tf | grep 'resource "tfe_workspace"' | awk -F '"' '{print $4}' > actual.txt
+skipped=$(echo "$(cat list.txt ; cat actual.txt)" | sort | uniq -u)
+echo "$(cat list.txt ; cat actual.txt)" | sort | uniq -u > skipped.txt
+
+while [ ! -z "$skipped"];do
+    
+    for i in $(cat skipped.txt)
+    do echo 'resource "tfe_workspace" "'$i'" { 
+    organization = "'$org_name'"  
+    name = "name"
+    }
+    ' >> main.tf;terraform import tfe_workspace.$i $i
+    done
+    if [ "$tier" = "Business" ]; then 
+        for i in $(jq -r '.resources[] .instances[] .attributes.agent_pool_id' terraform.tfstate)
+        do ws=$(grep -B6 $i terraform.tfstate | grep ws- | awk -F '"' '{print $4}'); sed  "s/resource \"tfe_workspace\" \"$ws\" {/resource \"tfe_workspace\" \"$ws\" { | agent_pool_id = \"$i\"/" main.tf | tr '|' '\n' > main1.tf
+        done
+        mv -f main1.tf main.tf
+    fi
+    terraform plan -no-color --destroy  > main1.tf 
+
+    echo -e 'provider "tfe" {} \r\n' > main2.tf
+    cat main1.tf | sed "s/ -//g" | sed "s/> null//g" | sed -E '/^ +id +=|^ +operations +=/d' | sed -E '/^tfe_workspace.ws/d' | sed -E '/^────|^Plan:|^Note:|^guarantee/d' | sed '/Terraform used the selected providers/,/perform the following actions:/d' | sed '/# /d' >> main2.tf. 
+    
+    mv -f main2.tf main.tf
+
+    rm -f main1.tf
+    
+
+    cat main.tf | grep 'resource "tfe_workspace"' | awk -F '"' '{print $4}' > actual.txt
+    skipped=$(echo "$(cat list.txt ; cat actual.txt)" | sort | uniq -u)
+    echo "$(cat list.txt ; cat actual.txt)" | sort | uniq -u > skipped.txt
+
+done
+
 end=`date +%s`
 
 runtime=$((end-start))
 
-echo -e 'Your configuration is ready. The time it took to do the imports was '$runtime' seconds. You have '$(cat list.txt | wc -l )' workspaces'
-
-
- 
+echo -e 'Your configuration is ready. The time it took to do the imports was '$(( $runtime / 60 ))' minutes and '$(( $runtime % 60 ))' seconds. You have '$(cat main.tf | grep 'resource "tfe_workspace"' | wc -l )' workspaces defined'
